09c698e0ca1668a5274f9a82d5b235671aa60361
nabechin <nabenabe0258@gmail.com>
2021-05-22 23:36:30 +0900

    2-7 need to reply

--- src/medium/2-7.ts
+++ src/medium/2-7.ts
@@ -1,1 +1,4 @@
-type Last<T extends any[]> = T extends [...any, infer T] ? T : never;
+// 配列の最後の要素を返却する
+// 1. スプレッド構文を使って、元の配列 = [...最後尾の要素を除いた配列, 最後の要素]の等式を作成.
+// 2. inferを使い、ジェネリクスパラメータに割り当てる配列に応じて要素の型を推論させてconditional typeで取得.
+type Last<T extends any[]> = T extends [...any, infer U] ? U : never;
