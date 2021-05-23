83d0a99337d484ad332224c60c21a3dcabcd0632
nabechin <nabenabe0258@gmail.com>
2021-05-23 17:20:47 +0900

    2-10 need to reply

--- src/medium/2-10.ts
+++ src/medium/2-10.ts
@@ -1,3 +1,7 @@
+// オブジェクトのプロパティの値を評価したいとき、U extends {propaty: value}で条件式を立てる.
+// 例えば、2つの目のジェネリクスパラメータが'dog'のとき、
+// {"dog": Dog}["dog"]
+// {property: value}[key]の形は、propertyとkeyの値が合致するvalueを取得する。
 type LookUp<U extends Animal, T extends string> = {
   [K in T]: U extends { type: T } ? U : never;
 }[T];
