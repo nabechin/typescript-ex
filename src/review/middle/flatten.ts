d7ac5ce9f2ced1baf92f0eef03919ce06e05694f
nabechin <nabenabe0258@gmail.com>
2021-08-21 15:04:12 +0900

    flatten done

---
+++ src/review/middle/flatten.ts
@@ -0,0 +1,8 @@
+type Flatten<T extends any[]> = T extends [infer L, ...infer R]
+  ? L extends unknown[]
+    ? [...Flatten<L>, ...Flatten<R>]
+    : [L, ...Flatten<R>]
+  : [];
+
+// [infer L, ... infer R]でRは何を示しているのか？
+// ex) [1, 2, [3, 4]]
